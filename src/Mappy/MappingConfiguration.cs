9791bbf2d9dd1e425d387019f3aaa09db81b40b1
Manojbabu <83714923+manusoft@users.noreply.github.com>
2025-05-18 13:48:38 +0400

    Add MappingConfiguration class for property mappings
    Introduces the `MappingConfiguration` class in the `Mappy` namespace. This class features a `PropertyMappings` dictionary for mapping source and destination types, and an `ExcludedProperties` hash set for tracking excluded properties. It includes methods to add property mappings and exclude specific properties from mapping.

---
+++ src/Mappy/MappingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿namespace Mappy;
+
+public class MappingConfiguration
+{
+    public Dictionary<(Type Source, Type Destination), Dictionary<string, string>> PropertyMappings { get; } = new();
+    public HashSet<(Type Source, Type Destination, string Property)> ExcludedProperties { get; } = new();
+
+    public void AddPropertyMapping(Type sourceType, Type destType, string sourceProp, string destProp)
+    {
+        var key = (sourceType, destType);
+        if (!PropertyMappings.ContainsKey(key))
+            PropertyMappings[key] = new Dictionary<string, string>();
+        PropertyMappings[key][sourceProp] = destProp;
+    }
+
+    public void ExcludeProperty(Type sourceType, Type destType, string propertyName)
+    {
+        ExcludedProperties.Add((sourceType, destType, propertyName));
+    }
+}
